ab5fd6337224d9af0a53cc1ed2a4689d16b809eb
MohamedAziz-Khezami <136236077+MohamedAziz-Khezami@users.noreply.github.com>
2024-10-20 11:37:30 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -253,46 +253,6 @@ export default function App() {
               </CardContent>
             </Card>
           </div>
-
-          {/* Line Chart */}
-          <div className="col-span-2 w-full">
-            <Card>
-              <CardHeader>
-                <CardTitle>Line Chart</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <ResponsiveContainer width="100%" height={300}>
-                  <LineChart data={chartData.lineData}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="value" stroke="#8884d8" />
-                  </LineChart>
-                </ResponsiveContainer>
-              </CardContent>
-            </Card>
-          </div>
-
-          {/* Area Chart */}
-          <div className="col-span-2 w-full">
-            <Card>
-              <CardHeader>
-                <CardTitle>Area Chart</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <ResponsiveContainer width="100%" height={300}>
-                  <AreaChart data={chartData.areaData}>
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Area type="monotone" dataKey="value" stroke="#8884d8" fill="#8884d8" />
-                  </AreaChart>
-                </ResponsiveContainer>
-              </CardContent>
-            </Card>
-          </div>
         </div>
       )}
     </div>
